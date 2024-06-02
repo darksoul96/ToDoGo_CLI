ff89aa0d9e7c1a2bf20fc3651c00f5033c26b3c9
Agustin F. Ruiz <62730262+darksoul96@users.noreply.github.com>
2024-06-02 03:41:34 -0300

    Added json file to read from

--- main.go
+++ main.go
@@ -1,9 +1,75 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 )
 
+type Task struct {
+	ID    int64  `json:"id"`
+	Title string `json:"title"`
+	Done  bool   `json:"done"`
+}
+
+type TaskList struct {
+	Tasks []Task `json:"tasks"`
+}
+
+func startupMessage() {
+	fmt.Println("Welcome back to To Do List CLI!")
+	fmt.Println("Type 'help' for more information")
+}
+
+func helpMessage() {
+	fmt.Println("Type 'clear' to clear the console")
+	fmt.Println("Type 'add' to add a new task")
+	fmt.Println("Type 'list' to list all tasks")
+	fmt.Println("Type 'done' to mark a task as done")
+	fmt.Println("Type 'delete' to delete a task")
+	fmt.Println("Type 'exit' to exit the program")
+
+}
+
+func loadTasks(taskList TaskList) {
+	file, err := os.OpenFile("tasks.json", os.O_RDONLY, 0644)
+
+	if err != nil {
+		panic(err)
+	} else {
+		defer file.Close()
+
+		//unmarshall json file
+		decoder := json.NewDecoder(file)
+		err = decoder.Decode(&taskList)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Loaded tasks from tasks.json")
+
+		//print tasks
+		for i, task := range taskList.Tasks {
+			fmt.Printf("%d. %s\n", i+1, task.Title)
+		}
+
+	}
+}
+
 func main() {
-	fmt.Println("hello")
+	var taskList TaskList
+
+	loadTasks(taskList)
+	startupMessage()
+
+	for {
+		var input string
+
+		fmt.Scanln(&input)
+		if input == "help" {
+			helpMessage()
+		} else if input == "exit" {
+			break
+		}
+	}
 }
